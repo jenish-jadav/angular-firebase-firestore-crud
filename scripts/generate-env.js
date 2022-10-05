81809dc838377b639cdacc782b75a1980fc39943
Jenish <jnish1112@gmail.com>
2022-10-05 19:56:34 +0530

    script updated

--- scripts/generate-env.js
+++ scripts/generate-env.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 let envFileContent = process.argv[2];
-//envFileContent=envFileContent.substring(1,envFileContent.length-1)
 fs.writeFileSync("src/environments/environment.prod.ts", envFileContent);
+fs.writeFileSync("src/environments/environment.ts", envFileContent);
